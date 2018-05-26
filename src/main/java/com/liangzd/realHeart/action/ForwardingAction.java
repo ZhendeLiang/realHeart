71281152a515381fd10cfd0716312f3b79f1f04c
LiangZhenDe <MrLeung@users.noreply.github.com>
2018-05-26 12:46:14 +0800

    配置转发Action

---
+++ src/main/java/com/liangzd/realHeart/action/ForwardingAction.java
@@ -0,0 +1,34 @@
+package com.liangzd.realHeart.action;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping(value="/filter")
+@Controller
+public class ForwardingAction {
+
+    private static final transient Logger log = LoggerFactory.getLogger(ForwardingAction.class);
+	
+//	@RequestMapping(value = "/login",method = RequestMethod.GET)
+//    public String login() {
+//        return "login";
+//    }
+
+	@RequestMapping(value = "/index",method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+//	@RequestMapping(value = "/403",method = RequestMethod.GET)
+//    public String errorPage() {
+//        return "403";
+//    }
+
+	@RequestMapping(value = "/adminIndex",method = RequestMethod.GET)
+    public String adminIndex() {
+        return "adminIndex";
+    }
+}
